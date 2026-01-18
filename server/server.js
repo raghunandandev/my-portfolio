c2873b0c2925e7079911496ede2e58d6a4fc74a0
raghunandandev <raghunandandevsingh@gmail.com>
2026-01-19 00:29:39 +0530

    merge f&b

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "http://localhost:3000", // Keep this for local development
-        "https://devraghunandan.vercel.app/" // Future deployed URL
+        "https://devraghunandan.vercel.app" // Future deployed URL
     ],
     credentials: true
 }));
